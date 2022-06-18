660ac912a236109661c62e433d9eab3dca95cc2d
matu36 <matipineda85@live.com.ar>
2022-06-18 00:21:24 -0300

    tareas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -124,9 +124,7 @@ function agregarSimboloExclamacion(str) {
 }
 
 function combinarNombres(nombre, apellido) {
-  // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
-  // Ejemplo: "Soy", "Henry" -> "Soy Henry"
-  // Tu código:
+ 
   var combinado = nombre + " " + apellido;
   return combinado;
 }
